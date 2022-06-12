b27d3020aa31028590160618e25e96ee0bde81ca
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-06-13 02:09:08 +0430

    adding tailwind

---
+++ tailwind.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [],
+    theme: {
+        colors: {
+            'sec-1': '#4d566e',
+            'sec-2':'#f1b139',
+            'white': 'white',
+            'black':'black'
+        },
+        maxWidth:{
+            '5': '20rem'
+        },
+        minHeight:{
+            '20':'20rem',
+            'screen-90':'90vh'
+        },
+
+        extend: {},
+    },
+    plugins: [],
+}
