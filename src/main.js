52b95dec8880a1327204062bb4b36a72a60ae8fa
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-06-13 02:10:17 +0430

    adding fontawesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// vue router
 import VueRouter from 'vue-router'
 import {routes} from './routes'
+
+// tailwind
+import '@/assets/css/tailwind.css'
+
+import '@/assets/css/home.css'
+// font awesome
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {library} from '@fortawesome/fontawesome-svg-core'
+// import {fas} from '@fortawesome/free-solid-svg-icons'
+import {faGithub, faFacebook, faReddit, faInstagram} from '@fortawesome/free-brands-svg-icons'
+// library.add(fas);
+library.add(faGithub, faFacebook, faReddit, faInstagram);
+Vue.component('fas', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
