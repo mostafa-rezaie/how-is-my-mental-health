cc26173f867a729782e25f2b09ad5ca586929e35
mostafa-rezaie <mostafa.rezaie@outlook.com>
2022-06-15 02:01:28 +0430

    Tests page

---
+++ src/components/CardTests.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card-container">
+    <div class="card">
+        <div class="text-2xl font-black text-sec-2">{{ testName }}</div>
+        <div class="text-white mt-5 text-lg">{{ testDescription }}</div>
+        <h2 class="mt-5 text-white">Number of Questions:{{ numOfQuestions }}</h2>
+        <h2 class="text-white">used Algorithms :{{ algorithms }}</h2>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardTest',
+  props: {
+    testName: String,
+    testDescription: String,
+    numOfQuestions: Number,
+    algorithms: String,
+
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  flex-direction: column;
+  height: 350px;
+  width: 100%;
+  min-width: 100px;
+  padding: .5rem 1.5rem .5rem 1rem;
+  border-radius: 1rem;
+  background: rgb(44, 62, 80);
+  box-shadow: -1rem 0 3rem black;
+  transition: 0.2s;
+}
+
+.card:hover {
+  transform: translateY(-1rem);
+}
+
+
+
+</style>
--- src/views/Tests.vue
+++ src/views/Tests.vue
@@ -1,16 +1,85 @@
 <template>
   <div>
-    this is a test
+    <Header/>
+    <div class="tests-text">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam amet blanditiis consequatur culpa cum dolor
+      doloribus, eaque exercitationem fuga illo, labore laboriosam laudantium libero magnam molestias mollitia nam
+      necessitatibus nostrum obcaecati omnis perspiciatis praesentium quae quam qui quidem quisquam quod reiciendis
+      repellat similique te
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet aperiam aspernatur at beatae consequuntur
+      dicta maiores nesciunt quisquam unde.
+    </div>
+    <div class="card-list">
+      <CardTest test-name="GHQ"
+                test-description="
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet
+                aperiam aspernatur abeatae consequuntur dicta maiores nesciunt quisquam unde."
+                num-of-questions=" 90"
+                algorithms=" SVM, Machine learning"
+
+      />
+      <CardTest test-name="CHARACTER STRENGTHS"
+                test-description="
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet
+                aperiam aspernatur abeatae consequuntur dicta maiores nesciunt quisquam unde."
+                num-of-questions=" 90"
+                algorithms=" SVM, Machine learning"
+
+      />
+      <CardTest test-name="HUMOR STYLES"
+                test-description="
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet
+                aperiam aspernatur abeatae consequuntur dicta maiores nesciunt quisquam unde."
+                num-of-questions=" 90"
+                algorithms=" SVM, Machine learning"
+
+      />
+      <CardTest test-name="BIG FIVE PERSONALITY"
+                test-description="
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet
+                aperiam aspernatur abeatae consequuntur dicta maiores nesciunt quisquam unde."
+                num-of-questions=" 90"
+                algorithms=" SVM, Machine learning"
+
+      />
+
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "../components/Header";
+import CardTest from "../components/CardTests";
+
 export default {
   name: 'Tests',
-  props: {
+  props: {},
+  components: {
+    Header,
+    CardTest,
   }
 }
 </script>
 
 <style scoped>
+.tests-text {
+  width: 80%;
+  color: white;
+  font-size: 1.2rem;
+  margin: 2rem auto 0 auto;
+}
+
+.card-list {
+  display: flex;
+  padding: 3rem;
+}
+
+.card-list div {
+  width: 25%;
+}
+
+.card-list :not(:first-child) {
+  /*margin-left: -1rem;*/
+  /*background-color: red;*/
+}
 </style>
