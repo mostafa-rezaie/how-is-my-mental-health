49556e240ae27c2b7f56cb4ace3f6a5d38107f52
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2022-07-20 12:09:34 +0430

    Forget password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="login-page">
-    <Header/>
-    <div class="card-container flex flex-col min-h-screen-90 justify-center m-auto w-1/3 relative">
-      <div class="circle">
-      </div>
-      <div class="card flex flex-col items-center min-h-20 ">
-        <div class="logo text-white text-3xl font-bold mt-5 ">
-          <span class="text-sec-2 ">Company</span> Logo
+    <Header />
+    <div
+      class="
+        card-container
+        flex flex-col
+        min-h-screen-90
+        justify-center
+        m-auto
+        w-1/3
+        relative
+      "
+    >
+      <div class="circle"></div>
+      <div class="card flex flex-col items-center min-h-20">
+        <div class="logo text-white text-3xl font-bold mt-5">
+          <span class="text-sec-2">Company</span> Logo
         </div>
         <div class="inputs mb-10 mt-10">
-          <user-input place-holder="Username" input-name="username"></user-input>
-          <user-input place-holder="Password" input-name="password" class="mt-3"></user-input>
+          <user-input
+            place-holder="Username"
+            input-name="username"
+          ></user-input>
+          <user-input
+            place-holder="Password"
+            input-name="password"
+            class="mt-3"
+          ></user-input>
         </div>
-        <div class="btn-container mb-10">
-          <button class="login-btn">Login!</button>
+        <div class="bottom-section">
+          <div class="btn-container mb-5">
+            <button class="btn">Login!</button>
+          </div>
+          <div class="forget-pass mb-5 text-center text-xs">
+            <a href="#" class="forget-pass-link">Forget Password?</a>
+          </div>
         </div>
       </div>
     </div>
@@ -25,18 +46,22 @@ import Header from "../components/Header";
 import UserInput from "../components/UserInput";
 
 export default {
-  name: 'Login',
+  name: "Login",
   props: {},
   components: {
     Header,
     UserInput,
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
 .card {
-  background: linear-gradient(135deg, rgba(44, 62, 80, 0.5), rgba(44, 62, 80, 0.5));
+  background: linear-gradient(
+    135deg,
+    rgba(44, 62, 80, 0.5),
+    rgba(44, 62, 80, 0.5)
+  );
   /*background-color: rgba(44, 62, 80, 0.5);*/
   /*filter: blur(10px);*/
   backdrop-filter: blur(5px);
@@ -46,7 +71,7 @@ export default {
 
 .circle {
   background-color: #f1b139;
-  clip-path: circle(30.0% at 50% 50%);
+  clip-path: circle(30% at 50% 50%);
   width: 300px;
   height: 300px;
   position: absolute;
@@ -54,20 +79,17 @@ export default {
   right: -120px;
   top: 50px;
 }
-
-.login-btn {
-  @apply px-4 py-2 border rounded-3xl text-base
-  border-sec-2 text-sec-2
-  ;
-  min-width: 150px;
-  font-weight: normal;
-  transition: all .3s ease;
+.forget-pass {
+  color: #4298b4;
+  transition: all 0.3s;
+  /* margin-bottom: 1.5rem; */
 }
-
-.login-btn:hover {
-  background-color: #f1b139;
-  color: black;
-  transition: all .3s ease-out;
-
+.forget-pass:hover {
+  color: #f1b139;
+}
+.forget-pass-link{
+  display: inline-block;
+  margin: auto;
+  text-align: center;
 }
 </style>
