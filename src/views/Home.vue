4c2c8673463595d98fdfa3251c88e8caa88f60ec
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2022-07-20 12:08:29 +0430

    change color pallet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,75 +1,80 @@
 <template>
   <div>
-    <Header/>
+    <Header />
     <div class="container text-center mt-8 mx-auto overflow-hidden">
       <div class="titles">
-        <div class="first-title text-lg ">How is my mental health</div>
-        <div class="quote text-8xl text-white font-bold">Calm yourself down</div>
-        <div class="second-title mt-3 text-sec-1">Take these test to evaluate your mental health</div>
+        <div class="first-title text-lg">How is my mental health</div>
+        <div class="quote text-8xl text-white font-bold">
+          Calm yourself down
+        </div>
+        <div class="second-title mt-3 text-white text-2xl">
+          Take these test to evaluate your mental health
+        </div>
       </div>
       <div class="button-container mt-8 flex max-w-5 m-auto justify-around">
         <button class="test-btn">Take a Test!</button>
-        <div>test</div>
-        <button class="login-btn">Login</button>
+        <!-- <button class="login-btn">Login</button> -->
       </div>
     </div>
     <div class="bottom-section absolute">
-      <div class="social-links absolute  ">
+      <div class="social-links absolute">
         <fas icon="fa-brands fa-instagram"></fas>
         <fas icon="fa-brands fa-github"></fas>
         <fas icon="fa-brands fa-reddit"></fas>
         <fas icon="fa-brands fa-facebook"></fas>
       </div>
-      <div class="image-container mx-auto mt-9 ">
-        <img src="@/assets/images/home-avatar.png" alt="">
+      <div class="image-container mx-auto mt-9">
+        <img src="@/assets/images/home-avatar.png" alt="" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Header,
   },
   beforeCreate() {
-    console.log(this.$route.path)
-    document.body.classList.add('body-home')
+    console.log(this.$route.path);
+    document.body.classList.add("body-home");
   },
   destroyed() {
-    document.body.classList.remove('body-home')
-  }
-}
+    document.body.classList.remove("body-home");
+  },
+};
 </script>
 
 <style>
 body {
 }
-
 </style>
 
 <style scoped>
-
 .first-title {
   color: #f1b139;
 }
 
-.test-btn, .login-btn {
+.test-btn,
+.login-btn {
   @apply px-4 py-2 border rounded-3xl text-base
-  border-sec-2 text-sec-2
-  ;
+   text-sec-1;
+  background-color: #f1b139;
+  border: 0;
   min-width: 150px;
   font-weight: normal;
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
 
-.test-btn:hover, .login-btn:hover {
-  background-color: #f1b139;
-  color: black;
-  transition: all .3s ease-out;
+.test-btn:hover,
+.login-btn:hover {
+  @apply bg-sec-2-darker ;
+  color: beige;
+  transition: all 0.3s ease-out;
+
 }
 
 .image-container {
@@ -89,7 +94,7 @@ body {
 
 .social-links svg {
   color: #f1b139;
-  margin-top: .5rem;
+  margin-top: 0.5rem;
 }
 
 .bottom-section {
@@ -100,6 +105,5 @@ body {
   .image-container {
     width: 50%;
   }
-
 }
 </style>
--- tailwind.js
+++ tailwind.js
@@ -4,8 +4,10 @@ module.exports = {
         colors: {
             'sec-1': '#4d566e',
             'sec-2':'#f1b139',
+            'sec-2-darker':'#d69e36',
             'white': 'white',
-            'black':'black'
+            'black':'black',
+            'red':'red'
         },
         maxWidth:{
             '5': '20rem'
