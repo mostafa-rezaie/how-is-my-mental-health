c52f938fc383fcd5c349b85f7fcf303ddc26f254
Mostafa <mostafa.rezaie@email.kntu.ac.ir>
2022-09-04 02:55:11 +0430

    not complete error and submit answers

--- src/views/TakeTest.vue
+++ src/views/TakeTest.vue
@@ -1,48 +1,90 @@
 <template>
   <div>
-    <Header/>
-    <router-view></router-view>
-    <div class="button-container w-full flex justify-center mb-10 ">
-
-    <button class="submit-btn mx-auto "><span class="hover-span"><fas icon="fa-solid fa-right-long"></fas></span><span class="default-text">Submit Answers!</span></button>
+    <Header />
+    <router-view @send-data="saveData"></router-view>
+    <div class="button-container w-full flex flex-col justify-center mb-10">
+      <button @click="sendAnswers" class="submit-btn mx-auto">
+        <span class="hover-span"><fas icon="fa-solid fa-right-long"></fas></span
+        ><span class="default-text">Submit Answers!</span>
+      </button>
+      <span v-if="isAnsweredAll" class="mx-auto text-red"
+        >Please answer to all questions!</span
+      >
     </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
+import axios from "axios";
+
 export default {
-  name: 'TakeTest',
-  props: {
+  name: "TakeTest",
+  props: {},
+  components: {
+    Header,
   },
-  components:{
-    Header
-  }
-}
+  data() {
+    return {
+      answers: [],
+      isAnsweredAll: false,
+    };
+  },
+  methods: {
+    saveData(ans) {
+      this.answers = ans;
+    },
+    sendAnswers() {
+      if (this.answers.length == 0) {
+        console.log("list is empty");
+        this.isAnsweredAll = true;
+      } else {
+        console.log("hey");
+        const options = {
+          url: "http://192.168.1.9:8000/tests-api/submit-answers",
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+            Authorization: "Token " + localStorage.getItem("token"),
+          },
+          data: {
+            questionnaireId: "3",
+            data: this.answers,
+          },
+        };
+        axios(options).then((res) => {
+          console.log(res.status);
+          if (res.status == 200) {
+            this.$router.push({ name: "home" });
+          }
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.submit-btn{
+.submit-btn {
   @apply px-4 py-2 border rounded-3xl text-base
-  border-sec-2 text-sec-2
-  ;
+  border-sec-2 text-sec-2;
   min-width: 150px;
   font-weight: normal;
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
-.submit-btn:hover{
+.submit-btn:hover {
   background-color: #f1b139;
   color: black;
-  transition: all .3s ease-out;
-
+  transition: all 0.3s ease-out;
 }
-.submit-btn:hover .default-text{
+.submit-btn:hover .default-text {
   display: none;
 }
-.submit-btn:hover .hover-span{
+.submit-btn:hover .hover-span {
   display: inline;
 }
-.hover-span{
+.hover-span {
   display: none;
 }
 </style>
